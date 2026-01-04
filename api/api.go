157fb8c70686629becae3f82a4680b2346ef1cad
souzannatha <souzanatha04@gmail.com>
2026-01-04 11:14:10 -0300

    feat: adicionado funcionalidade ao POST.

--- api/api.go
+++ api/api.go
@@ -1,6 +1,11 @@
 package api
 
 import (
+	"encoding/json"
+	"log/slog"
+	"math/rand/v2"
+	"net/url"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 
@@ -26,9 +31,34 @@ type Response struct {
 	Data  any    `json:"error,omitempty"`
 }
 
+func sendJSON(w http.ResponseWriter, resp Response, status int) {
+	w.Header().Set("Content-Type", "application/json")
+	data, err := json.Marshal(resp)
+	if err != nil {
+		slog.Error("error ao fazer marshal de json", "error", err)
+		sendJSON(w, Response{Error: "something went wrong"}, http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(status)
+	if _, err := w.Write(data); err != nil {
+		slog.Error("error ao enviar a resposta:", "error", err)
+		return
+	}
+}
+
 func handlePost(db map[string]string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-
+		var body PostBody
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			sendJSON(w, Response{Error: "invalid Body"}, http.StatusUnprocessableEntity)
+			return
+		}
+		if _, err := url.Parse(body.URL); err != nil {
+			sendJSON(w, Response{Error: "invalid url passed"}, http.StatusBadRequest)
+		}
+		code := genCode()
+		db[code] = body.URL
+		sendJSON(w, Response{Data: code}, http.StatusCreated)
 	}
 }
 
@@ -36,4 +66,15 @@ func handleGet(db map[string]string) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 	}
+}
+
+const characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+
+func genCode() string {
+	const n = 8
+	byts := make([]byte, 8)
+	for i := range n {
+		byts[i] = characters[rand.IntN(len(characters))]
+	}
+	return string(byts)
 }
